5d08b21b9788da1ba0165705bd229a5880877167
Lediaaga <127754948+Lediaaga@users.noreply.github.com>
2023-06-02 17:08:54 +0200

    Add files via upload

---
+++ politike.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>politike</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+  
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+
+
+<section class="header">
+
+   <a href="kreu.php" class="logo">shqip news</a>
+
+   <nav class="navbar">
+      <a href="kreu.php">kreu</a>
+      <a href="aktualitet.php">aktualitet</a>
+      <a href="politike.php">politike</a>
+      <a href="lajmi.php">pjese e lajmit</a>
+      <a href="adminlogin.php">admin login</a>
+   </nav>
+
+</section>
+
+
+<div class="heading" style="background:url(img/kuvendi.jpg) no-repeat">
+   <h1>politike</h1>
+</div>
+
+
+
+<section class="packages">
+
+   <h1 class="heading-title">me e fundit e politikes</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>PD e Foltorja mbledhin deputetët/ Rishfaqet Basha, sot aktivitet me të rinjtë. Bejko sulmon Tabakun</h3>
+            <p>Pak ditë pas zgjedhjeve të 14 majit, nisin lëvizjet mes demokratëve. PD e Foltorja e Berishës kanë njoftuar sot se do të mbledhin grupet parlametare, mbledhje e parashikuar për t’u zhvilluar në të njëjtën orë, por ai që ndryshon është vendi.Një javë pas dorëheqjes së Enkelejd Alibeajt nga drejtimi i partisë, gazetarja Erneta Shevroja tha për ABC News se PD nuk e ka ende një emër për kryetarin e komanduar, ndërkohë që është Gazment Bardhi ai që po thërret grupin parlamentar. PD zyrtare nuk e ka ende një kryetar, prej një jave Jorida Tabaku nuk ka marrë asnjë vendim mbi hapat që do të ndjekë, nëse do të marrë apo jo drejtimin e partisë.
+</p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Senatorët amerikanë vizitë në Tiranë, Kim reagon duke cituar Murphy: SHBA nuk ka mik më të mirë se Shqipëria</h3>
+            <p>Në kuadër të një turi rajonal, senatorët e SHBA Chris Murphy dhe Gary Peters vizituan ditën e sotme Shqipërinë.Ata zhvilluan takime me presidentin, Bajram Begaj dhe me kryeministrin Edi Rama e në këto takime ishin të shoqëruar nga ambasadorja e SHBA në Tiranë, Yuri Kim. Kim ka reaguar së fundmi në lidhje me këtë vizitë të senatorëve ku ka cituar Murphy, i cili më parë reagoi në Twitter duke shkruar se SHBA nuk ka mik të mirë në botë se Shqipëria, ndërsa vetë im shton së bashkëpunimi mes vendeve është më i rëndësishëm se kurrë.
+
+</p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>“SHBA nuk ka mik më të mirë në botë se Shqipëria”, Senatori amerikan reagon pas takimit me Ramën</h3>
+            <p>Senatori amerikan, Chris Murphy së bashku me disa senatorë të tjerë kanë vizituar ditën e sotme Shqipërinë në kuadër të një turi rajonal.Gjatë gjithë takimeve ata shoqëroheshin nga ambasadorja e SHBA në Shqipëri, Yuri Kim.Përpos takimit me presidentin Begaj, senatorët janë takuar edhe me kryeministrin Edi Rama, e pas këtij takimi ka reaguar në Twitter, Murphy.Ai shkruan se ka qenë fantastike të kalonte pak kohë me mikun e tij, Ramën dhe shton se SHBA nuk ka mik më të mirë në botë se Shqipëria.
+
+            </p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Shumë zhurmë…por mbaroi/ Retorikë, por asnjë aksion diplomatik. Pas zgjedhjeve, ulen armët?</h3>
+            <p>Në takimin e Brukselit, ministrja e Jashtme shqiptare këmbënguli, se çështja Beleri është vetëm çështje penale. Prokurorët e SPAK-ut i konsideruan bindëse provat që konfirmojë se Fredi Beleri ka blerë vota për 14 majin, duke bërë që drejtësia shqiptare t’i lexojë faktet njëlloj. Beleri mbeti ne burg. Kjo bëri që këtë të martë pasdite përpara ambasadës shqiptarë në Athinë të protestohet nga mbrojtës të kreut himariot të Bashkisë.</p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Humbja, Berisha jep takim në 2025/ Mbledh kryesinë e “Foltores”, por “fajin” e ka Edi Ramën</h3>
+            <p>Pak më shumë se një javë nga 14 maji, humbja e thellë dhe për me tepër humbja e kontrollit politik në të gjitha bashkitë e republikës, Sali Berisha thirri mbledhjen e kryesisë së tij.  Por jo për një analizë për atë që u ka ndodhur, por si gjithmonë për të akuzuar Edi Ramën se vodhi zgjedhjet në një mënyrë që duket se e ka pare vetëm ai.“Kjo është hera e parë në histori që vota nuk blihet më nën dorë, por blihet haptas. Publikisht me 670 mijë zarfe, secili prej tyre me 50 mijë lekë në mes të fushatës elektorale dorëzuar çdo pensionisti shqiptar”, u shpreh Berisha.</p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="img/politike6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Rama mesazh kryebashkiakëve: Të majtë apo të djathtë qofshin, qytetarët të kenë derën e hapur</h3>
+            <p>Kryetari i PS-së, Edi Rama vijoi turin e falënderimeve për rezultatin e arritur në zgjedhjet e 14 majit. Nga Lezha, Rama tha se qytetarët kanë votuar me mendje të ftohtë duke zgjedhur punën dhe jo premtimet boshe. Ai kërkoi nga kryebashkiakët të jenë korrekt dhe të punojnë ngushtësisht me qytetarët për të trajtuar e zgjidhur nevojat e tyre.“Të zgjedhurit duhet të kuptojnë se njerëzit kanë një pritshmëri shumë të lartë. Ka të mirat e vjetra, se të jep vota, por ka hallin nga mbrapa, sepse duhet të shlyhet me rezultate që u përkasin pritshmërive. Duhet njerzillëku i kryetarëve, nënkryetarëve, këshilltarëve dhe të gjithë atyre që janë në kontakt me qytetarin”, u shpreh Rama..</p>
+            <a href="lajmi.php" class="btn">shto dicka</a>
+         </div>
+      </div>
+
+    
+
+</section>
+
+
+
+<section class="footer">
+
+<div class="box-container">
+
+   <div class="box">
+      <h3>quick links</h3>
+      <a href="kreu.php"> <i class="fas fa-angle-right"></i> kreu</a>
+      <a href="aktualitet.php"> <i class="fas fa-angle-right"></i> aktualitet</a>
+      <a href="politike.php"> <i class="fas fa-angle-right"></i> politike</a>
+      <a href="lajmi.php"> <i class="fas fa-angle-right"></i> showbizz</a>
+   </div>
+
+   <div class="box">
+      <h3>contact info</h3>
+      <a href="#"> <i class="fas fa-phone"></i> +355 00 00 000 </a>
+      <a href="#"> <i class="fas fa-phone"></i> +355 11 11 111 </a>
+      <a href="#"> <i class="fas fa-envelope"></i> shqipnews@gmail.com </a>
+      <a href="#"> <i class="fas fa-map"></i> tirana, albania </a>
+   </div>
+
+   <div class="box">
+      <h3>follow us</h3>
+      <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+      <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+      <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+   </div>
+
+</div>
+
+<div class="credit"> created by <span>Epoka University</span> | all rights reserved! </div>
+
+</section>
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
