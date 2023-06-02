65a42246847a80cc185f0280843c7ddff05f8b96
KleaMerko <128406196+KleaMerko@users.noreply.github.com>
2023-06-02 16:17:58 +0200

    Add files via upload

--- adminpanel.php
+++ adminpanel.php
@@ -13,13 +13,11 @@ if(!isset($_SESSION['AdminLoginId'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>adminpanel</title>
 
-   <!-- swiper css link  -->
+
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
